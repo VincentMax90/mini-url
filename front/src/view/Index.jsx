76b10c5a1382d823c59b57950ae7331270c51a5c
VincentMax90 <maxclc@hotmail.com>
2024-01-15 18:31:22 -0300

    index app funcional

--- front/src/view/Index.jsx
+++ front/src/view/Index.jsx
@@ -1,13 +1,45 @@
-import React from "react";
-
-
+import React, { useState } from "react";
+import axios from "axios";
 
 const Index = () => {
+  const [url, setUrl] = useState("");
+  const [miniUrl, setMiniUrl] = useState("");
+
+  const handleGenerateMiniUrl = async () => {
+    try {
+      const response = await axios.post("http://localhost:3001/api/url/create", {
+        url: url,
+      });
+
+      setMiniUrl(response.data.url.miniUrl);
+    } catch (error) {
+      console.error("Error generando Mini URL:", error.message);
+    }
+  };
+console.log(miniUrl)
   return (
     <>
-      {" "}
-      <div>Bienvenido a miniurl</div>
-     
+      <div>
+        <h1>Generador de Mini URL</h1>
+        <label>
+          Ingresa la URL:
+          <input
+          placeholder="https://www.ejemplo.com/"
+            type="text"
+            value={url}
+            onChange={(e) => setUrl(e.target.value)}
+          />
+        </label>
+        <button onClick={handleGenerateMiniUrl}>Generar Mini URL</button>
+        {miniUrl && (
+          <div>
+            <p>Esta es la Mini URL:</p>
+            <a href={miniUrl} target="_blank" rel="noopener noreferrer">
+              {miniUrl}
+            </a>
+          </div>
+        )}
+      </div>
     </>
   );
 };
